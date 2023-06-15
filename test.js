b246bf38951b3e5e3f85f66ca0402b9818eda5e1
Christian Kessler IV <cgkio@users.noreply.github.com>
2023-06-15 13:29:58 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 var Gpio = require('pigpio').Gpio;
 
 // Define the GPIO pin that you've connected to the sensor
-var sensorPinNo1 = 18;
-var sensorPinNo2 = 24;
-var sensorPinNo3 = 1;
-var sensorPinNo4 = 12;
+var sensorPinNo1 = 18; // change this to the pin you are using
+var sensorPinNo2 = 23; // change this to the pin you are using
+var sensorPinNo3 = 24; // change this to the pin you are using
+var sensorPinNo4 = 25; // change this to the pin you are using
 
 // Create new Gpio objects for the sensors
 var sensor1 = new Gpio(sensorPinNo1, {
@@ -33,11 +33,17 @@ var sensor4 = new Gpio(sensorPinNo4, {
 
 // Create a function that can be used for all sensors
 function handleInterrupt(sensor, previousState) {
+  var debounceTimeout = null;
+
   sensor.on('interrupt', function (level) {
-    if (level !== previousState) {
-      console.log(`Sensor on pin ${sensor.gpio} changed: ${level === 0 ? 'Door is open' : 'Door is closed'}`);
-      previousState = level;
-    }
+    if(debounceTimeout) clearTimeout(debounceTimeout);
+
+    debounceTimeout = setTimeout(function() {
+      if (level !== previousState) {
+        console.log(`Sensor on pin ${sensor.gpio} changed: ${level === 0 ? 'Door is open' : 'Door is closed'}`);
+        previousState = level;
+      }
+    }, 100); // 100 ms debounce period
   });
 }
 
