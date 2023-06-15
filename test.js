1a41d715e92e8ecf936373508edac01fc93ac423
Christian Kessler IV <cgkio@users.noreply.github.com>
2023-06-15 12:55:00 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,58 +1,20 @@
-const rpio = require("rpio");
-const axios = require("axios");
-const macaddress = require("macaddress");
-const firebaseAdmin = require("firebase-admin");
-const moment = require("moment");
-const Gpio = require("pigpio").Gpio;
-const Airtable = require("airtable");
+var Gpio = require('pigpio').Gpio;
 
-let doLogStuff = true;
-let isOpen = null;
-let oldIsOpen = null;
-let timestampBuffer = [];
+// Define the GPIO pin that you've connected to the NO point on the sensor
+var sensorPinNo = 18; // change this to the pin you are using
 
-rpio.init({
-  gpiomem: true,
+// Create a new Gpio object for the sensor
+var sensor = new Gpio(sensorPinNo, {
+  mode: Gpio.INPUT,
+  pullUpDown: Gpio.PUD_UP, // Set the internal pull-up resistor
+  edge: Gpio.EITHER_EDGE // Detect both rising and falling edges
 });
 
-// Raspberry Pi 4 pin assignments
-const DOOR_SENSOR_PIN = 12; // magnetic contact switch (door sensor)
-// const DOOR_2_SENSOR_PIN = 12; // magnetic contact switch (door sensor)
-// const DOOR_3_SENSOR_PIN = 12; // magnetic contact switch (door sensor)
-// const DOOR_4_SENSOR_PIN = 12; // magnetic contact switch (door sensor)
-
-// Initialize the GPIO pins
-rpio.init({ gpiomem: false });
-rpio.open(DOOR_SENSOR_PIN, rpio.INPUT, rpio.PULL_UP);
-// rpio.open(DOOR_SENSOR_PIN, rpio.INPUT, rpio.PULL_DOWN);
-
-// functions to handle debugging logs
-function log(message) {
-  if (doLogStuff === true) {
-    console.log(message);
+// Detect door open or close based on the GPIO pin state
+sensor.on('interrupt', function(level) {
+  if (level === 0) { // Falling edge means door closed
+    console.log("Door is closed");
+  } else { // Rising edge means door open
+    console.log("Door is open");
   }
-}
-
-// function that polls the door sensor every 100 milliseconds for open/close status
-function pollSensor() {
-    oldIsOpen = isOpen;
-    isOpen = rpio.read(DOOR_SENSOR_PIN);
-    console.log(isOpen);
-    if (!isOpen && isOpen !== oldIsOpen) {
-      log("Door One - OPEN"); // door has been detected to be open
-      timestampBuffer.push(Date.now()); // add first timestamp to the buffer for when the door opened
-    } else if (isOpen !== oldIsOpen) {
-      log("Door One - CLOSED"); // door has been detected to be closed
-      timestampBuffer.push(Date.now()); // add the current timestamp to the buffer as the door closed time
-      // const openTimestamp = new Date(timestampBuffer[0]).toISOString();
-      // const closeTimestamp = new Date(timestampBuffer[1]).toISOString();
-      // const boardingDuration = (lastPassengerTimestamp - firstPassengerTime) / 1000;
-      // log(timestampBuffer)
-      // log(openTimestamp);
-      // log(closeTimestamp);
-      // log(boardingDuration);
-    }
-    setTimeout(pollSensor, 1000);
-  }
-
-pollSensor();
+});
